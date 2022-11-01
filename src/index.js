8751e5d197f520a54da65f50fe43cb8b4e0e4c27
Simon Elvery <simon@elvery.net>
2022-11-01 11:11:00 +1000

    Remove legacy (pre-PL) functionality and use range decoys

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
-import { whenDOMReady } from "@abcnews/env-utils";
-import { ensureBlockMount, exactMountSelector } from "@abcnews/mount-utils";
+import { requestDOMPermit } from "@abcnews/env-utils";
+
+if (process.env.NODE_ENV === "development") {
+  console.debug(`[simple-remove-tags] public path: ${__webpack_public_path__}`);
+  // TODO: Add this back once https://github.com/abcnews/env-utils/pull/26 lands
+  // mockDecoyActivationEvents();
+}
 
 // Inserts CSS into document.head with a <style> element
-const insertCSS = css => {
+const insertCSS = () => {
+  const css = ".simple-remove-tags-hidden {display: none !important;}";
   const el = document.createElement("style");
-
   if (el.styleSheet) {
     el.styleSheet.cssText += css;
   } else {
@@ -16,65 +21,14 @@ const insertCSS = css => {
   document.querySelector("head").appendChild(el);
 };
 
-// Repeats a string a number of times (concatenated)
-const repeatString = (string, times) => {
-  let repeatedString = "";
-
-  while (times > 0) {
-    repeatedString += string;
-    times--;
-  }
-
-  return repeatedString;
-};
-
 // 1. Wait for the document to be ready (including Presentation Layer hydration)
-whenDOMReady.then(() => {
-  // 2. Grab references to all #remove/#end remove mount points
-  const openingMounts = [
-    ...document.querySelectorAll(exactMountSelector("remove"))
-  ];
-  const closingMounts = [
-    ...document.querySelectorAll(exactMountSelector("endremove"))
-  ];
-
-  // 3. Determine the largest number of elements between each pair of opening
-  // and closing mount points (or remaining elements after an unclosed opener)
-  const maxElementsBetweenMounts = openingMounts.reduce((max, openingMount) => {
-    let numElementsBetweenMounts = 0;
-    let nextElement = openingMount;
-
-    while (
-      ((nextElement = nextElement.nextElementSibling),
-      nextElement !== null && closingMounts.indexOf(nextElement) === -1)
-    ) {
-      numElementsBetweenMounts++;
-    }
-
-    return Math.max(max, numElementsBetweenMounts);
-  }, 0);
-
-  // 4. Convert any a[id] & a[name] mounts to div[id][data-mount]
-  [...openingMounts, ...closingMounts].forEach(ensureBlockMount);
-
-  // 5. Create and insert a stylesheet that hides the mount points, and all
-  // elements between, by adding enough sibling selectors to account for the
-  // largest number between:
-  //
-  //   <style>
-  //     #remove[data-mount],
-  //     #remove[data-mount] + :not(#endremove),
-  //     #remove[data-mount] + :not(#endremove) + :not(#endremove),
-  //     ...,
-  //     #endremove[data-mount] {
-  //       display: none;
-  //     }
-  //   </style>
-  //
-  insertCSS(
-    [...Array(maxElementsBetweenMounts + 1).keys()]
-      .map(i => `#remove[data-mount]${repeatString(" + :not(#endremove)", +i)}`)
-      .concat("#endremove[data-mount] { display: none !important }")
-      .join()
-  );
+requestDOMPermit("remove").then((els) => {
+  if (Array.isArray(els)) {
+    insertCSS();
+    els.forEach((el) => el.classList.add("simple-remove-tags-hidden"));
+  } else {
+    console.warn(
+      "This version of simple-remove-tags is only compatible with PL rendered pages"
+    );
+  }
 });
